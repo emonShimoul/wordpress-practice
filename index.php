b1add1f95d3b4daa0d19610e650cb6bde3180c29
emonShimoul <emonshimoul@gmail.com>
2023-12-26 05:45:07 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* This template is for displaying the header
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
