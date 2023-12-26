963bed84a557cc20a4dd58c92888fb43a01c8466
emonShimoul <emonshimoul@gmail.com>
2023-12-26 06:01:46 +0600

    basic structure of a wp theme

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
 <!DOCTYPE html>
 <html lang="<?php language_attributes(); ?>" class="no-js">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?> >
     
+
+<?php wp_footer(); ?>
 </body>
 </html>
